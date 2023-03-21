028e4d8c6bae5a81aa51077d6d3f29caa0436057
Leo Bergqvist <leo@bergqvist.se>
2023-03-21 13:19:53 +0100

    highlight on win + hover

--- scripts/4-in-a-row.js
+++ scripts/4-in-a-row.js
@@ -5,15 +5,20 @@ const cell_size = 80;
 let current_player = "red";
 let game_matrix = [];
 
+let game_over = false;
+
 function create_board() {
     for (let i = 0; i < rows; i++) {
         let row = [];
         for (let j = 0; j < columns; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
+
+
             cell.onmouseover = () => handle_mouse_over(j);
             cell.onmouseout = () => handle_mouse_out(j);
-            cell.onclick = () => handleClick(j);
+            cell.onclick = () => handle_click(j);
+
             const token = document.createElement("div");
             token.classList.add("token", "hidden");
             cell.appendChild(token);
@@ -25,16 +30,21 @@ function create_board() {
 }
 
 
-function handleClick(column) {
+function handle_click(column) {
+    if (game_over) {
+        return
+    };
+
     let target_row = find_first_empty_cell(column);
     if (target_row !== -1) {
         game_matrix[target_row][column].classList.remove("hidden");
         game_matrix[target_row][column].classList.add(current_player);
+
         if (check_victory(target_row, column)) {
             setTimeout(() => {
                 alert(current_player.toUpperCase() + " wins!");
-                reset_board();
-            }, 500);
+                game_over = true;
+            }, 25);
         } else {
             current_player = current_player === "red" ? "yellow" : "red";
             update_hover_tokens();
@@ -53,21 +63,31 @@ function find_first_empty_cell(column) {
 }
 
 function check_victory(row, column) {
-    return check_direction(row, column, 1, 0) ||
-        check_direction(row, column, 0, 1) ||
-        check_direction(row, column, 1, 1) ||
-        check_direction(row, column, 1, -1);
+    return (
+        check_direction(row, column, 1, 0, true) ||
+        check_direction(row, column, 0, 1, true) ||
+        check_direction(row, column, 1, 1, true) ||
+        check_direction(row, column, 1, -1, true)
+    );
 }
 
-function check_direction(row, column, row_dir, col_dir) {
+function check_direction(row, column, row_dir, col_dir, highlight = false) {
     let count = 0;
     for (let i = -3; i <= 3; i++) {
-        let new_row = row + i * row_dir;
-        let new_col = column + i * col_dir;
-        if (new_row >= 0 && new_row < rows && new_col >= 0 && new_col < columns &&
-            game_matrix[new_row][new_col].classList.contains(current_player)) {
+        let newRow = row + i * row_dir;
+        let newCol = column + i * col_dir;
+        if (
+            newRow >= 0 &&
+            newRow < rows &&
+            newCol >= 0 &&
+            newCol < columns &&
+            game_matrix[newRow][newCol].classList.contains(current_player)
+        ) {
             count++;
             if (count === 4) {
+                if (highlight) {
+                    highlight_winning_tokens(row, column, row_dir, col_dir);
+                }
                 return true;
             }
         } else {
@@ -77,44 +97,55 @@ function check_direction(row, column, row_dir, col_dir) {
     return false;
 }
 
-function reset_board() {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            game_matrix[i][j].classList.remove("red", "yellow", "hidden");
-        }
+function highlight_winning_tokens(row, column, rowDir, colDir) {
+    for (let i = 0; i < 4; i++) {
+        let new_row = row + i * rowDir;
+        let new_col = column + i * colDir;
+        game_matrix[new_row][new_col].classList.add("winning");
     }
-    current_player = "red";
 }
 
-const hoverRow = document.getElementById("hoverRow");
+const hover_row = document.getElementById("hover_row");
 
 function create_hover_tokens() {
     for (let j = 0; j < columns; j++) {
         const token = document.createElement("div");
-        token.classList.add("hoverToken", "hidden", current_player);
-        hoverRow.appendChild(token);
+        token.classList.add("hover_token", "hidden", current_player);
+        hover_row.appendChild(token);
     }
 }
 
 function update_hover_tokens() {
     for (let j = 0; j < columns; j++) {
-        hoverRow.children[j].classList.remove("red", "yellow");
-        hoverRow.children[j].classList.add(current_player);
+        hover_row.children[j].classList.remove("red", "yellow");
+        hover_row.children[j].classList.add(current_player);
     }
 }
-
-
 function handle_mouse_over(column) {
-    hoverRow.children[column].classList.remove("hidden");
+    if (game_over) {
+        return
+    };
+    hover_row.children[column].classList.remove("hidden");
 }
 
 function handle_mouse_out(column) {
-    hoverRow.children[column].classList.add("hidden");
+    hover_row.children[column].classList.add("hidden");
 }
+
+function reset_board() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            game_matrix[i][j].classList.remove("red", "yellow", "hidden", "winning");
+        }
+    }
+    current_player = "red";
+    game_over = false;
+    update_hover_tokens();
+}
+
+const restart_button = document.getElementById("restart_button");
+
+restart_button.addEventListener("click", reset_board);
 
 create_hover_tokens();
 create_board();
-
-const restart_button = document.getElementById("restartButton");
-
-restart_button.addEventListener("click", reset_board);
