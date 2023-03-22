ab466b3428777a4b51b2dbab84a90a07c9d6c93c
Leo Bergqvist <leo@bergqvist.se>
2023-03-22 13:26:40 +0100

    finished connect 4 + home  page + started on rps

---
+++ scripts/rock_paper_scissors.js
@@ -0,0 +1,38 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function play(userChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    const result = document.getElementById('result');
+
+    if (playerScore < 3 && computerScore < 3) {
+        if (userChoice === computerChoice) {
+            result.textContent = `It's a tie! Score: You - ${playerScore}, Computer - ${computerScore}`;
+        } else if (
+            (userChoice === 'rock' && computerChoice === 'scissors') ||
+            (userChoice === 'paper' && computerChoice === 'rock') ||
+            (userChoice === 'scissors' && computerChoice === 'paper')
+        ) {
+            playerScore++;
+            result.textContent = `You win this round! ${userChoice} beats ${computerChoice}. Score: You - ${playerScore}, Computer - ${computerScore}`;
+        } else {
+            computerScore++;
+            result.textContent = `You lose this round! ${computerChoice} beats ${userChoice}. Score: You - ${playerScore}, Computer - ${computerScore}`;
+        }
+
+        if (playerScore === 3) {
+            result.textContent += " Congratulations! You won the game.";
+        } else if (computerScore === 3) {
+            result.textContent += " Sorry, computer won the game.";
+        }
+    } else {
+        return;
+    }
+}
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    result.textContent = "Score: You - 0, Computer - 0";
+}
