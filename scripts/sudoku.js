e2f90712e5763bd13f3fe599ebc29cdff47d99b2
Leo Bergqvist <leo@bergqvist.se>
2023-04-28 11:33:36 +0200

    Soduko win, remove input, adjusted buttons

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -4,8 +4,8 @@ for (let i = 0; i < 81; i++) {
     cell.classList.add('s_cell');
     const row_index = Math.floor(i / 9);
     const col_index = i % 9;
-    cell.setAttribute("data-row", row_index);
-    cell.setAttribute("data-col", col_index);
+    cell.setAttribute("data-row", row_index.toString());
+    cell.setAttribute("data-col", col_index.toString());
     sudoku_board.appendChild(cell);
 }
 let selected_cell;
@@ -16,29 +16,32 @@ cells.forEach((cell, index) => {
 });
 // Function to select a cell
 function select_cell(cell, index) {
-    if (cell.textContent !== "") {
-        // Skip cells that are pre-filled with numbers
-        return;
+    if (cell.classList.contains("player_input") || cell.textContent === "") {
+        if (selected_cell && selected_cell !== cell) {
+            selected_cell.classList.remove("selected");
+        }
+        selected_cell = cell;
+        selected_cell.classList.add("selected");
+        // Add a keydown event listener to handle number input
+        document.addEventListener("keydown", handle_number_input);
     }
-    if (selected_cell) {
-        selected_cell.classList.remove("selected");
-    }
-    selected_cell = cell;
-    selected_cell.classList.add("selected");
-    // Add a keydown event listener to handle number input
-    document.addEventListener("keydown", handle_number_input);
 }
 // Function to handle number input
 function handle_number_input(event) {
-    if (selected_cell && !isNaN(event.key) && Number(event.key) >= 1 && Number(event.key) <= 9) {
+    if (selected_cell && ((event.key >= 1 && event.key <= 9) || event.key === "Backspace" || event.key === "Delete")) {
         const row_index = Number(selected_cell.getAttribute("data-row"));
         const col_index = Number(selected_cell.getAttribute("data-col"));
-        if (is_input_valid(event.key, row_index, col_index)) {
-            selected_cell.textContent = event.key;
+        if (event.key === "Backspace" || event.key === "Delete") {
+            selected_cell.textContent = "";
         }
-        // Perform any necessary checks (e.g., validate input, check if the puzzle is solved)
-        // After performing the checks, remove the keydown event listener
-        document.removeEventListener("keydown", handle_number_input);
+        else if (is_input_valid(event.key, row_index, col_index)) {
+            selected_cell.textContent = event.key;
+            selected_cell.classList.add("player_input"); // Add the 'player_input' class to the cell
+            if (check_win()) {
+                // Display a win message or perform any other desired actions upon winning
+                alert("Congratulations, you have won!");
+            }
+        }
     }
 }
 const puzzles = {
@@ -94,5 +97,33 @@ function is_input_valid(value, row_index, col_index) {
         }
     }
     // If the input passes all checks, it is valid according to Sudoku rules
+    return true;
+}
+function check_win() {
+    // Convert the cells NodeList to a 2D array (9x9) for easier row/column/grid access
+    const board_array = Array.from(cells).reduce((acc, cell, index) => {
+        const row = Math.floor(index / 9);
+        const col = index % 9;
+        if (!acc[row]) {
+            acc[row] = [];
+        }
+        acc[row][col] = cell.textContent;
+        return acc;
+    }, []);
+    // Check all rows, columns, and grids for valid Sudoku solutions
+    for (let i = 0; i < 9; i++) {
+        let row = new Set();
+        let col = new Set();
+        let grid = new Set();
+        for (let j = 0; j < 9; j++) {
+            row.add(board_array[i][j]);
+            col.add(board_array[j][i]);
+            grid.add(board_array[Math.floor(i / 3) * 3 + Math.floor(j / 3)][(i % 3) * 3 + (j % 3)]);
+        }
+        if (row.size !== 9 || col.size !== 9 || grid.size !== 9) {
+            return false;
+        }
+    }
+    // If all checks pass, the player has won
     return true;
 }
