65f769619af83b993e814098089e1e096c84f02a
Leo Bergqvist <leo@bergqvist.se>
2023-05-11 13:12:04 +0200

    instructions modal, about , contact

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,23 @@ function update_site_location(target) {
 }
 ;
 document.addEventListener("DOMContentLoaded", function () {
-    // Drop Down Function
-    const menuButton = document.getElementById('menuButton');
-    menuButton.addEventListener('change', function () {
-        (menuButton.checked) ? (document.querySelector('.the-bass').classList.add('dropped')) : (document.querySelector('.the-bass').classList.remove('dropped'));
+    const menu_btn = document.getElementById('menuButton');
+    menu_btn.addEventListener('change', function () {
+        (menu_btn.checked) ? (document.querySelector('.the-bass').classList.add('dropped')) : (document.querySelector('.the-bass').classList.remove('dropped'));
+    });
+    const gameModal = document.getElementById("gameModal");
+    const closeButton = document.querySelector(".close");
+    function openModal() {
+        gameModal.style.display = "block";
+    }
+    document.querySelector(".modal_btn").addEventListener("click", openModal);
+    function closeModal() {
+        gameModal.style.display = "none";
+    }
+    closeButton.addEventListener("click", closeModal);
+    window.addEventListener("click", function (event) {
+        if (event.target === gameModal) {
+            closeModal();
+        }
     });
 });
--- scripts/index.ts
+++ scripts/index.ts
@@ -3,16 +3,35 @@ function update_site_location(target: string) {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-
-
-    // Drop Down Function
-    const menuButton = document.getElementById('menuButton') as HTMLInputElement;
-    menuButton.addEventListener('change', function () {
-        (menuButton.checked) ? (
+    const menu_btn = document.getElementById('menuButton') as HTMLInputElement;
+    menu_btn.addEventListener('change', function () {
+        (menu_btn.checked) ? (
             document.querySelector('.the-bass').classList.add('dropped')
         ) : (
             document.querySelector('.the-bass').classList.remove('dropped')
         );
     });
 
+
+
+    const gameModal = document.getElementById("gameModal") as HTMLElement;
+    const closeButton = document.querySelector(".close") as HTMLElement;
+
+    function openModal() {
+        gameModal.style.display = "block";
+    }
+
+    document.querySelector(".modal_btn").addEventListener("click", openModal);
+
+
+    function closeModal() {
+        gameModal.style.display = "none";
+    }
+
+    closeButton.addEventListener("click", closeModal);
+    window.addEventListener("click", function (event) {
+        if (event.target === gameModal) {
+            closeModal();
+        }
+    });
 });
